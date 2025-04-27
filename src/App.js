d697f1aeb6b0fb02515e9de482c2ae83916083a0
NaveenKumar <120005658+nkr1320@users.noreply.github.com>
2025-04-27 17:40:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function calculateBMI() {
+    const height = document.getElementById('height').value;
+    const weight = document.getElementById('weight').value;
+    const bmiResult = document.getElementById('bmiResult');
+
+    if (height && weight) {
+      const heightInMeter = height / 100;
+      const bmi = (weight / (heightInMeter * heightInMeter)).toFixed(2);
+      bmiResult.innerText = `Your BMI is: ${bmi}`;
+    } else {
+      bmiResult.innerText = `Enter valid input`;
+    }
+
+    // Clear the result after 5 seconds
+    setTimeout(() => {
+      bmiResult.innerText = '';
+      
+    }, 5000);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='bmi'>
+        <div className='bmicontainer'>
+          <h1 id="header">BMI CALCULATOR</h1>
+          <input type='text' id='height' placeholder='Enter your height (cm)' />
+          <input type='text' id='weight' placeholder='Enter your weight (kg)' />
+          <button id="calcBtn" onClick={calculateBMI}>Calculate BMI</button>
+          <h2 id="bmiResult"></h2>
+        </div>
+      </div>
+    </>
   );
 }
 
